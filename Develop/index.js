36e80f9a30fc6492eba71d74a395048faa23c36b
Becca Doan <rebecca.doan@cirrus.com>
2022-07-09 12:03:57 -0500

    added an initial prompt

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,103 @@
 // TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require('inquirer')
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileContent) {
 
-// TODO: Create a function to initialize app
-function init() {}
+}
 
-// Function call to initialize app
-init();
+const promptUser = () => {
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: "What is the name of the project you're creating a README for?",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter project name');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: "Please describe your project",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please describe your project');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'installation',
+                message: "What are the steps to install this project?",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please provide installation instructions');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: "Provide usage examples and instructions",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter usage instructions');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'contribution',
+                message: "Please describe how others can contribute to your project",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter contribution instructions');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'tests',
+                message: "Please add your test instructions here",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter test instructions');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'list',
+                name: 'license',
+                message: 'You can add a license to this project here',
+                choices: ['Apache', 'MIT', 'IBM', 'BSD']
+              },
+            
+        ])
+}
+
+
+promptUser().then(userResponse => {
+    writeToFile('README.md', userResponse)
+})
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,26 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licenseInfo = {
+    Apache: {
+      badge: 'https://img.shields.io/badge/License-Apache_2.0-blue.svg',
+      url: 'https://opensource.org/licenses/Apache-2.0'
+    }, 
+    IBM: {
+      badge: 'https://img.shields.io/badge/License-IPL_1.0-blue.svg',
+      url: 'https://opensource.org/licenses/IPL-1.0'
+    }, 
+    BSD: {
+      badge: 'https://img.shields.io/badge/License-BSD_3--Clause-blue.svg',
+      url: 'https://opensource.org/licenses/BSD-3-Clause'
+    }, 
+    MIT: {
+      badge: 'https://img.shields.io/badge/License-MIT-yellow.svg',
+      url: 'https://opensource.org/licenses/MIT'
+    }
+  }
+  return [licenseInfo.license]
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,9 +32,42 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  ${license}
+
+  ## How to Contribute
+
+  ${data.contribution}
+
+  ## Tests
+
+  ${data.tests}
 `;
 }
 
 module.exports = generateMarkdown;
+
+
